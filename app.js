dfed4bac8163b7c1e64d98d021ad71a9e1341116
Gabriela <gabrielacacosta@gmail.com>
2024-10-31 22:54:20 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const nameInput = document.getElementById("name");
+const edadInput = document.getElementById("edad");
+const emailInput = document.getElementById("email");
+const phoneInput = document.getElementById("phone");
+const messageInput = document.getElementById("message");
+
+function saveData() {
+    if (nameInput.value == "" || edadInput.value == ""|| emailInput.value == "" || phoneInput.value == "" || messageInput.value == "") {
+        alert("Llena todos los datos, por favor");
+    } else if(parseInt(edadInput.value) < 18){
+        alert("Solo usuarios mayores de 18 años");
+    }else{
+        alert(`            Nombre Completo: ${nameInput.value}
+            Edad: ${edadInput.value}
+            Email: ${emailInput.value}
+            Teléfono: ${phoneInput.value}
+            Encribe tu consulta : ${messageInput.value}`)
+    } 
+}
